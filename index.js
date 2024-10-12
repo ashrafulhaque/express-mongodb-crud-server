0ed2f90148f6a4fbdadb12a04a24b78f23c9cfb6
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-12 22:05:36 +0600

    Added post functionality to the server

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// bootcamp_db_user
-// YsLPXpTh8E2KsKW5
-
 const uri =
   "mongodb+srv://bootcamp_db_user:YsLPXpTh8E2KsKW5@cluster0.q1o0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,24 +21,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (don't close the connection here)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const userCollection = client.db("usersDbBootcamp").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //console.log(user);
+      const insertedUser = await userCollection.insertOne(user);
+      res.send(insertedUser);
+    });
+
+    // Ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log("Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the express mongodb crud server!");
+  res.send("Welcome to the express MongoDB CRUD server!");
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
